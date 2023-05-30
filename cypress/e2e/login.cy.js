c0a1cc12af1f2eeb97ad5613a92b5fdba14bce7b
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-29 21:15:13 -0300

    Adicionando casos de teste de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,57 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Tela de Login', () => {
+    
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('#user_login').as('usuario')
+    cy.get('#user_password').as('senha')
+    cy.get('input[type="submit"][value="Sign in"]').as('sign in')
   })
-})
+
+  it('login com usuário vazio', () => { 
+    cy.get('@senha')
+      .type('Codornar', {log: false}, {delay: 0});
+    cy.get('@sign in')
+      .click()
+    cy.get('p.gl-field-error')
+      .should('be.visible', 'This field is required.')
+  });
+
+  it('login com senha vazia', () => {
+    cy.get('@usuario')
+      .type('root', {delay: 0})
+    cy.get('@sign in')
+      .click()
+    cy.get('p.gl-field-error')
+      .should('be.visible', 'This field is required.')
+  });
+
+  it('usuário e senha inválidos', () => {
+    cy.get('@usuario')
+      .type('teste', {delay: 0})
+    cy.get('@senha')
+      .type('teste', {delay: 0})
+    cy.get('@sign in')
+      .click()
+    cy.get('.flash-alert > span')
+      .should('be.visible', 'Invalid Login or password.')
+    cy.get('@senha')
+      .should('not.have.value')
+  });
+
+  it('verificar checkbox', () => {
+    cy.get('input[type="checkbox"][value="1"]')
+      .should('have.length', 1) 
+      .check()
+      .should('be.checked', 'Remember me')
+      .uncheck()
+      .should('not.be.checked')
+  });
+
+  // it.only('clicar no botão "esqueceu sua senha?"', () => {
+  //   cy.get('a[href*="/users/password/new"]')
+  //     .click()
+  //     .contains('h1', 'GitLab Community Edition')
+  // });
+  
+
+});
